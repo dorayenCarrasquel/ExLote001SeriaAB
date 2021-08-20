11aa000478115eecd12b753bde5908d15999b672
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-08-20 14:28:20 -0300

    Criando diretorios Clube Serie A

--- Ejercicio1.bat
+++ Ejercicio1.bat
@@ -1,3 +1,13 @@
 md "Todos os Clubes"
 cd "Todos os Clubes"
+
 md "Serie A" "Serie B"
+cd "Serie A"
+
+md "Atletico-MG" "Palmeiras" "Fortaleza" "Bragantino" "Flamengo"
+
+md "Athletico-PR" "Atletico-GO" "Ceara" "Internacional" "Santos"
+
+md "Corinthians" "Juventude" "Bahia" "Sao Paulo" "Fluminense" "Cuiaba"
+
+md "Sport" "America-MG" "Gremio" "Chapecoense"
