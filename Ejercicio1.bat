a2886b40734a9274e953458e4d12c9966c8e8eda
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-08-20 14:32:50 -0300

    Criando diretorios Clube Serie B

--- Ejercicio1.bat
+++ Ejercicio1.bat
@@ -11,3 +11,15 @@ md "Athletico-PR" "Atletico-GO" "Ceara" "Internacional" "Santos"
 md "Corinthians" "Juventude" "Bahia" "Sao Paulo" "Fluminense" "Cuiaba"
 
 md "Sport" "America-MG" "Gremio" "Chapecoense"
+
+cd..
+
+cd "Serie B"
+
+md "Coritiba" "Goias" "Avai" "CRB" "Sampaio Correa"
+
+md "Guarani" "Nautico" "Botafogo" "Operario-PR" "CSA"
+
+md "Vasco" "Remo" "Brusque" "Cruzeiro" "Vila Nova"
+
+md "Ponte Preta" "Londrina" "Vitoria" "Confianca" "Brasil de Pelotas"
